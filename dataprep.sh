0eb3b1154ccf5f5abaa6962768cf1474bd3d8be4
miguel <lamahmichelmarie@gmail.com>
2024-01-06 00:52:18 +0100

    06 Janv 2023 00:52

---
+++ dataprep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=dataprep_ecapa
+#SBATCH --partition=gpu
+#SBATCH --time=7-00:00:00
+# #SBATCH --mem=60GB
+# #SBATCH --cpus-per-task=20
+#SBATCH --output=dataprep_output.log
+#SBATCH --error=dataprep_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+python3 ./dataprep.py --save_path data --download --user USERNAME --password PASSWORD
+#python3 ./dataprep.py --save_path data --extract
+#python3 ./dataprep.py --save_path data --convert
+#python3 ./dataprep.py --save_path data --augment
+
+conda deactivate
