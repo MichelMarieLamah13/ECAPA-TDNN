726d6a5a807bb81224db0ea82e6bf42f6a7dcfde
miguel <lamahmichelmarie@gmail.com>
2024-01-12 20:22:32 +0100

    12 Janv 2024 20:22

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_ecapa
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=128GB
 #SBATCH --cpus-per-task=20
