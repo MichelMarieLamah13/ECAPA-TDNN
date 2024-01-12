3752fbdfae8db38869f28ba500e50fa639818b63
miguel <lamahmichelmarie@gmail.com>
2024-01-12 04:14:21 +0100

    12 Janv 2024 04:14

--- model.py
+++ model.py
@@ -153,7 +153,7 @@ class ECAPA_TDNN(nn.Module):
             )
             self.specaug = FbankAug()  # Spec augmentation
         else:
-            self.learnable_weights = nn.Parameter(torch.zeros(13, 768))  # 13 couches: CNN + 12 transformers
+            self.learnable_weights = nn.Parameter(torch.ones(13, 768))  # 13 couches: CNN + 12 transformers
             self.wav2vec2 = CustomWav2Vec2Model()
 
         self.conv1 = nn.Conv1d(self.feat_dim, C, kernel_size=5, stride=1, padding=2)
