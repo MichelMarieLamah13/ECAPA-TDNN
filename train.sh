2e317ab2598c7dadaa1fb069a07d7ea508b582ba
mmlamah <lamahmichelmarie@gmail.com>
2024-01-15 14:31:51 +0100

    15 janv 2024 14:31

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=train_ecapa
 #SBATCH --partition=gpu
-# #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
+#SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 # #SBATCH --gres=gpu:nvidia_a100-pcie-40gb:1
-#SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
+# #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=20
