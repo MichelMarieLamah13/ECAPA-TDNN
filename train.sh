430039ef2d913c599f62ebd5bdc0d4e675a5ab60
miguel <lamahmichelmarie@gmail.com>
2024-01-15 06:39:33 +0100

    15 Janv 2024 06:39

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@
 # #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 #SBATCH --gres=gpu:nvidia_a100-pcie-40gb:1
 #SBATCH --time=7-00:00:00
-#SBATCH --mem=128GB
+#SBATCH --mem=64GB
 #SBATCH --cpus-per-task=20
 #SBATCH --output=train_output.log
 #SBATCH --error=train_error.log
