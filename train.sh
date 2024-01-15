b7ed0a08d3f922ef97a480ee879d2c3e51ef0b25
miguel <lamahmichelmarie@gmail.com>
2024-01-15 06:53:07 +0100

    15 Janv 2024 06:53

--- train.sh
+++ train.sh
@@ -2,10 +2,11 @@
 #SBATCH --job-name=train_ecapa
 #SBATCH --partition=gpu
 # #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
-#SBATCH --gres=gpu:nvidia_a100-pcie-40gb:1
+# #SBATCH --gres=gpu:nvidia_a100-pcie-40gb:1
+#SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
-# #SBATCH --mem=16GB
-#SBATCH --cpus-per-task=5
+# #SBATCH --mem=32GB
+#SBATCH --cpus-per-task=20
 #SBATCH --output=train_output.log
 #SBATCH --error=train_error.log
 
@@ -13,7 +14,7 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModel.py --save_path exps/exp3 --feat_type fbank --feat_dim 80 --n_cpu 5
+python3 trainECAPAModel.py --save_path exps/exp3 --feat_type fbank --feat_dim 80 --n_cpu 20
 #python3 trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80
 #python3 -m pdb trainECAPAModel.py --save_path exps/exp1
 
