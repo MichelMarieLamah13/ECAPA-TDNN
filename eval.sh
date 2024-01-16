b61ac2f808b693b9cb925e4aed09f4c157d138c0
miguel <lamahmichelmarie@gmail.com>
2024-01-16 15:27:41 +0100

    16 Janv 2024 15:27

--- eval.sh
+++ eval.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=eval_ecapa
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
-#SBATCH --time=7-00:00:00
-#SBATCH --mem=128GB
-#SBATCH --cpus-per-task=20
+#SBATCH --time=10-00:00:00
+#SBATCH --mem=100GB
+#SBATCH --cpus-per-task=10
 #SBATCH --output=eval_output.log
 #SBATCH --error=eval_error.log
 
@@ -12,8 +12,8 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-# python3  trainECAPAModel.py --eval --initial_model exps/exp4/model/model_0001.model  --feat_type wav2vec2 --feat_dim 768
+# python3  trainECAPAModel.py --eval --initial_model exps/exp4/model/model_0001.model  --feat_type wav2vec2 --feat_dim 768 --n_cpu 10
 # python3 -m pdb trainECAPAModel.py --eval --initial_model exps/exp4/model/model_0001.model  --feat_type wav2vec2 --feat_dim 768
-# python3 trainECAPAModel.py --eval --initial_model exps/pretrain.model --feat_type fbank --feat_dim 80
+# python3 trainECAPAModel.py --eval --initial_model exps/pretrain.model --feat_type fbank --feat_dim 80 --n_cpu 10
 
 conda deactivate
--- train.sh
+++ train.sh
@@ -4,9 +4,9 @@
 #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 # #SBATCH --gres=gpu:nvidia_a100-pcie-40gb:1
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
-#SBATCH --time=7-00:00:00
-#SBATCH --mem=128GB
-#SBATCH --cpus-per-task=50
+#SBATCH --time=10-00:00:00
+#SBATCH --mem=100GB
+#SBATCH --cpus-per-task=10
 #SBATCH --output=train_output.log
 #SBATCH --error=train_error.log
 
@@ -14,7 +14,7 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModel.py --save_path exps/exp3 --feat_type fbank --feat_dim 80 --n_cpu 50
+python3 trainECAPAModel.py --save_path exps/exp3 --feat_type fbank --feat_dim 80 --n_cpu 10
 #python3 trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80
 #python3 -m pdb trainECAPAModel.py --save_path exps/exp1
 
