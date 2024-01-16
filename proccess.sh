849f086852fbc9b4367844ab001687c75e3fb28d
miguel <lamahmichelmarie@gmail.com>
2024-01-16 05:17:24 +0100

    16 Janv 2024 05:17

---
+++ proccess.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=process_ecapa
+# #SBATCH --partition=gpu
+# #SBATCH --gres=gpu:1
+#SBATCH --time=7-00:00:00
+# #SBATCH --mem=128GB
+#SBATCH --output=process_ecapa_output.log
+#SBATCH --error=process_ecapa_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+rm -rf data
+
+conda deactivate
