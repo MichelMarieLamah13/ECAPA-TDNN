8f6d8f321bea942dbc0378992ebbd1ac0de0fe63
miguel <lamahmichelmarie@gmail.com>
2024-01-16 10:55:16 +0100

    16 Janv 2024 10:55

--- train_wav2vec2.sh
+++ train_wav2vec2.sh
@@ -12,7 +12,7 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModel.py --save_path exps/exp4 --feat_type wav2vec2 --feat_dim 768
+python3 trainECAPAModel.py --save_path exps/exp4 --feat_type wav2vec2 --feat_dim 768 --n_cpu 60
 # python3 trainECAPAModel.py --save_path exps/exp2 --feat_type wav2vec2 --feat_dim 768
 #python3 -m pdb trainECAPAModel.py --save_path exps/exp2
 
