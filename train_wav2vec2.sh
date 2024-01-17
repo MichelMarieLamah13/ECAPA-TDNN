79fa4211e7d462406d23297067850f042f82ae82
miguel <lamahmichelmarie@gmail.com>
2024-01-17 14:36:54 +0100

    17 Janv 2024 14:36

--- train_wav2vec2.sh
+++ train_wav2vec2.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 #SBATCH --time=10-00:00:00
 #SBATCH --mem=80GB
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=8
 #SBATCH --output=train_wav2vec2_output.log
 #SBATCH --error=train_wav2vec2_error.log
 
@@ -12,6 +12,6 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModel.py --save_path exps/exp2 --feat_type wav2vec2 --feat_dim 768 --n_cpu 10
+python3 trainECAPAModel.py --save_path exps/exp2 --feat_type wav2vec2 --feat_dim 768 --n_cpu 8
 
 conda deactivate
