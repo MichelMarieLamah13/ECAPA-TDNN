6df00d4f6241e123a73dcb5c64268dca6563cb14
miguel <lamahmichelmarie@gmail.com>
2024-01-17 04:29:33 +0100

    17 Janv 2023 04:29

--- eval.sh
+++ eval.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 #SBATCH --time=10-00:00:00
-#SBATCH --mem=100GB
-#SBATCH --cpus-per-task=10
+#SBATCH --mem=128GB
+#SBATCH --cpus-per-task=50
 #SBATCH --output=eval_output.log
 #SBATCH --error=eval_error.log
 
@@ -12,7 +12,7 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-# python3  trainECAPAModel.py --eval --initial_model exps/exp4/model/model_0001.model  --feat_type wav2vec2 --feat_dim 768 --n_cpu 10
+# python3  trainECAPAModel.py --eval --initial_model exps/exp4/model/model_0001.model  --feat_type wav2vec2 --feat_dim 768 --n_cpu 50
 # python3 -m pdb trainECAPAModel.py --eval --initial_model exps/exp4/model/model_0001.model  --feat_type wav2vec2 --feat_dim 768
 # python3 trainECAPAModel.py --eval --initial_model exps/pretrain.model --feat_type fbank --feat_dim 80 --n_cpu 10
 
--- train_wav2vec2.sh
+++ train_wav2vec2.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 #SBATCH --time=10-00:00:00
-#SBATCH --mem=100GB
-#SBATCH --cpus-per-task=10
+#SBATCH --mem=128GB
+#SBATCH --cpus-per-task=50
 #SBATCH --output=train_wav2vec2_output.log
 #SBATCH --error=train_wav2vec2_error.log
 
@@ -12,7 +12,7 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModel.py --save_path exps/exp4 --feat_type wav2vec2 --feat_dim 768 --n_cpu 10
+python3 trainECAPAModel.py --save_path exps/exp4 --feat_type wav2vec2 --feat_dim 768 --n_cpu 50
 # python3 trainECAPAModel.py --save_path exps/exp2 --feat_type wav2vec2 --feat_dim 768
 #python3 -m pdb trainECAPAModel.py --save_path exps/exp2
 
