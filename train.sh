fd87d1f90fdc6041706c580d85b015209e81ffed
miguel <lamahmichelmarie@gmail.com>
2024-01-22 21:34:47 +0100

    22 Janv 2024 21:34

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@
 # #SBATCH --gres=gpu:nvidia_a100-pcie-40gb:1
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 #SBATCH --gres=gpu:1
-#SBATCH --exclude=apollon,eris
+#SBATCH --exclude=apollon,eris,helios,alpos
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
 #SBATCH --cpus-per-task=10
