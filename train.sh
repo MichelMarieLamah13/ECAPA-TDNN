9f301f7801670565af914ec3657255acd5a1b033
miguel <lamahmichelmarie@gmail.com>
2024-01-22 16:09:23 +0100

    22 Janv 2024 16:09

--- train.sh
+++ train.sh
@@ -5,10 +5,11 @@
 # #SBATCH --gres=gpu:nvidia_a100-pcie-40gb:1
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 #SBATCH --gres=gpu:1
+#SBATCH —exclude=apollon,eris
 #SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=10
 #SBATCH --output=bf_output.log
 #SBATCH --error=bf_error.log
 
@@ -16,6 +17,6 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 8 --batch_size 256
+python3 trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 256
 
 conda deactivate
--- train_wav2vec2.sh
+++ train_wav2vec2.sh
@@ -4,10 +4,11 @@
 # #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 #SBATCH --gres=gpu:1
+#SBATCH —exclude=apollon,eris
 #SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=10
 #SBATCH --output=bw_output.log
 #SBATCH --error=bw_error.log
 
@@ -15,6 +16,6 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModel.py --save_path exps/exp2 --feat_type wav2vec2 --feat_dim 768 --n_cpu 8 --batch_size 256
+python3 trainECAPAModel.py --save_path exps/exp2 --feat_type wav2vec2 --feat_dim 768 --n_cpu 10 --batch_size 256
 
 conda deactivate
