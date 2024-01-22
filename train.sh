7b583029d70aa88d1900ab74d4a7d47525095e16
miguel <lamahmichelmarie@gmail.com>
2024-01-22 17:06:19 +0100

    22 Janv 2024 17:06

--- train.sh
+++ train.sh
@@ -8,7 +8,7 @@
 #SBATCH --exclude=apollon,eris
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=8
 #SBATCH --output=bf_output.log
 #SBATCH --error=bf_error.log
 
@@ -16,6 +16,6 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 256
+python3 trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 8 --batch_size 256
 
 conda deactivate
--- train_wav2vec2_2d.sh
+++ train_wav2vec2_2d.sh
@@ -7,7 +7,7 @@
 #SBATCH --exclude=apollon,eris
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=8
 #SBATCH --output=bw2d_output.log
 #SBATCH --error=bw2d_error.log
 
@@ -15,6 +15,6 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModel.py --save_path exps/exp3 --feat_type wav2vec2 --feat_dim 768 --n_cpu 10 --batch_size 256 --is_2d
+python3 trainECAPAModel.py --save_path exps/exp3 --feat_type wav2vec2 --feat_dim 768 --n_cpu 8 --batch_size 256 --is_2d
 
 conda deactivate
