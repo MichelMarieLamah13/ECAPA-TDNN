6dc63c12b8055496a7f18777f2123b1121ab4e80
miguel <lamahmichelmarie@gmail.com>
2024-01-23 09:54:13 +0100

    23 Janv 2024 09:54

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=bf
 #SBATCH --partition=gpu
 # #SBATCH --gres=gpu:1
-# #SBATCH --gres=gpu:gtx_titan_x:1
-#SBATCH --gres=gpu:rtx_3090:1
+#SBATCH --gres=gpu:gtx_titan_x:1
+# #SBATCH --gres=gpu:rtx_3090:1
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 #SBATCH --exclude=apollon,helios,alpos
 #SBATCH --time=7-00:00:00
