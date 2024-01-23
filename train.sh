749276d33a08e7987e812c4aec47b10eafb5a509
miguel <lamahmichelmarie@gmail.com>
2024-01-23 09:44:36 +0100

    23 Janv 2024 09:44

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=bf
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+# #SBATCH --gres=gpu:1
 # #SBATCH --gres=gpu:gtx_titan_x:1
 # #SBATCH --gres=gpu:rtx_3090:1
-# #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
+#SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 #SBATCH --exclude=apollon,helios,alpos
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
@@ -16,7 +16,7 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 -m pdb trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
-# python3 trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
+# python3 -m pdb trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
+python3 trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
 
 conda deactivate
