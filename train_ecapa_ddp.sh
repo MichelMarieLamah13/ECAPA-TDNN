277ebbbe6afdc301857d9d955fc703aef45645ab
miguel <lamahmichelmarie@gmail.com>
2024-01-30 18:40:06 +0100

    30 Janv 2024 18:40

--- train_ecapa_ddp.sh
+++ train_ecapa_ddp.sh
@@ -13,7 +13,7 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-# python3 trainECAPAModelDDP.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
-python3 trainECAPAModelDDP.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
+python3 trainECAPAModelDDP.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128 --master_port 54323
+# python3 trainECAPAModelDDP.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
 
 conda deactivate
