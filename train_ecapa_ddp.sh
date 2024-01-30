3c219730cb71d7118128b39d57ef98b3820026a1
miguel <lamahmichelmarie@gmail.com>
2024-01-30 09:11:14 +0100

    30 Janv 2024 09:11

---
+++ train_ecapa_ddp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=bf_ddp
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=GPURAM_Max_16GB
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=32GB
+#SBATCH --cpus-per-task=10
+#SBATCH --output=%x_output.log
+#SBATCH --error=%x_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+python3 trainECAPAModelDDP.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
+
+conda deactivate
---
+++ train_wav2vec2_ddp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=bw_1_ddp
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=GPURAM_Max_16GB
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=32GB
+#SBATCH --cpus-per-task=10
+#SBATCH --output=%x_output.log
+#SBATCH --error=%x_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+python3 trainECAPAModelDDP.py --save_path exps/exp2_1_ddp --feat_type wav2vec2 --n_cpu 10 --batch_size 128 --model_name facebook/wav2vec2-large-960h
+
+conda deactivate
