d53d5d46a7dea5e85e2004eb37fc1c93e92c2507
miguel <lamahmichelmarie@gmail.com>
2024-02-01 15:14:33 +0100

    01 Fev 2024 15:14

--- ECAPAModel.py
+++ ECAPAModel.py
@@ -273,9 +273,10 @@ class ECAPAModelDDP(nn.Module):
         self.disable_tqdm = self.gpu_id != 0
 
         self.learnable_weights = None
+        self.feat_type = feat_type
         self.is_2d = is_2d
         self.find_unused_parameters = False
-        if feat_type == 'wav2vec2':
+        if self.feat_type == 'wav2vec2':
             self.find_unused_parameters = True
             wav2vec2 = CustomWav2Vec2Model(model_name=model_name)
             n_layers, feat_dim = wav2vec2.get_output_dim()
@@ -284,9 +285,13 @@ class ECAPAModelDDP(nn.Module):
                     torch.zeros(n_layers, feat_dim))  # 13 couches: CNN + 12 transformers
             else:
                 self.learnable_weights = nn.Parameter(torch.ones(n_layers))
+        elif self.feat_type == 'wavlm':
+            wavlm = CustomWavLMModel(model_name=model_name)
+            n_layers, feat_dim = wavlm.get_output_dim()
+            self.learnable_weights = nn.Parameter(torch.ones(n_layers))
 
         # ECAPA-TDNN
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device(f"cuda:{self.gpu_id}" if torch.cuda.is_available() else "cpu")
         self.speaker_encoder = ECAPA_TDNN(C=C, feat_type=feat_type, feat_dim=feat_dim, model_name=model_name).to(
             self.gpu_id)
         self.speaker_encoder = DDP(
@@ -319,12 +324,9 @@ class ECAPAModelDDP(nn.Module):
             self.zero_grad()
             labels = torch.LongTensor(labels).to(self.gpu_id)
             # labels = torch.LongTensor(labels)
-            if self.learnable_weights is not None:
-                speaker_embedding = self.speaker_encoder(data.to(self.gpu_id), aug=True,
-                                                         learnable_weights=self.learnable_weights,
-                                                         is_2d=self.is_2d)
-            else:
-                speaker_embedding = self.speaker_encoder(data.to(self.gpu_id), aug=True)
+            speaker_embedding = self.speaker_encoder(data.to(self.gpu_id), aug=True,
+                                                     learnable_weights=self.learnable_weights,
+                                                     is_2d=self.is_2d)
 
             # speaker_embedding = self.speaker_encoder.forward(data, aug=True)
             nloss, prec = self.speaker_loss(speaker_embedding, labels)
@@ -378,16 +380,13 @@ class ECAPAModelDDP(nn.Module):
                 data_1 = data_1.to(self.gpu_id)
                 data_2 = all_data_2[i].to(self.gpu_id)
                 with torch.no_grad():
-                    if self.learnable_weights is None:
-                        embedding_1 = self.speaker_encoder(data_1, aug=False)
-                        embedding_2 = self.speaker_encoder(data_2, aug=False)
-                    else:
-                        embedding_1 = self.speaker_encoder(data_1, aug=False,
-                                                           learnable_weights=self.learnable_weights,
-                                                           is_2d=self.is_2d)
-                        embedding_2 = self.speaker_encoder(data_2, aug=False,
-                                                           learnable_weights=self.learnable_weights,
-                                                           is_2d=self.is_2d)
+                    embedding_1 = self.speaker_encoder(data_1, aug=False,
+                                                       learnable_weights=self.learnable_weights,
+                                                       is_2d=self.is_2d)
+                    embedding_2 = self.speaker_encoder(data_2, aug=False,
+                                                       learnable_weights=self.learnable_weights,
+                                                       is_2d=self.is_2d)
+
                     embedding_1 = F.normalize(embedding_1, p=2, dim=1)
                     embedding_2 = F.normalize(embedding_2, p=2, dim=1)
                 embeddings[file] = [embedding_1, embedding_2]
