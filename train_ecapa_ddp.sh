abb4cd575b937007b0601fc894276686258d4410
miguel <lamahmichelmarie@gmail.com>
2024-02-23 14:35:29 +0100

    23 Fev 2024 14:35

--- train_ecapa_ddp.sh
+++ train_ecapa_ddp.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=b_fbank
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
-# #SBATCH --constraint=GPURAM_Min_24GB
+#SBATCH --constraint=GPURAM_Min_16GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=64GB
 #SBATCH --cpus-per-task=16
@@ -13,6 +13,6 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainECAPAModelDDP.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 16 --batch_size 128 --master_port 54323
+python3 trainECAPAModelDDP.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 16 --batch_size 256 --master_port 54323
 
 conda deactivate
