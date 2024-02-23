dc4c7513adf5855906df8351c966194ac926bbe8
miguel <lamahmichelmarie@gmail.com>
2024-02-23 23:30:45 +0100

    23 Fev 2024 23:30

--- train_ecapa_ddp.sh
+++ train_ecapa_ddp.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=b_fbank
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
-#SBATCH --constraint=GPURAM_Min_16GB
+#SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=64GB
 #SBATCH --cpus-per-task=16
