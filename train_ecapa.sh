ac6fbfc75aabe3b11c4e97beed14bbc6743dabb8
miguel <lamahmichelmarie@gmail.com>
2024-02-23 05:01:14 +0100

    23 Fev 2023 05:01

--- train_ecapa.sh
+++ train_ecapa.sh
@@ -5,7 +5,7 @@
 #SBATCH --constraint=GPURAM_Min_24GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=20
 #SBATCH --output=%x_output.log
 #SBATCH --error=%x_error.log
 
@@ -15,6 +15,6 @@ conda activate ecapa_tdnn
 
 # python3 -m pdb trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
 # python3 trainECAPAModel.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
-python3 trainECAPAModel.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 32 --batch_size 512
+python3 trainECAPAModel.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 20 --batch_size 512
 
 conda deactivate
