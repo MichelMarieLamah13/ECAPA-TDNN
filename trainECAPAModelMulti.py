3412d8706612ebe8abf818dc14f0ce837983407d
miguel <lamahmichelmarie@gmail.com>
2024-02-24 02:17:31 +0100

    24 Fev 2024 02:17

---
+++ trainECAPAModelMulti.py
@@ -0,0 +1,48 @@
+"""
+This is the main code of the ECAPATDNN project, to define the parameters and build the construction
+"""
+
+import argparse, glob, os, torch, warnings, time
+import sys
+
+import yaml
+from torch.utils.data import DataLoader
+
+from tools import *
+from dataLoader import train_loader
+from ECAPAModel import ECAPAModel
+
+
+def init_eer(score_path):
+    errs = []
+    with open(score_path) as file:
+        lines = file.readlines()
+        for line in lines:
+            parteer = line.split(',')[-1]
+            parteer = parteer.split(' ')[-1]
+            parteer = parteer.replace('%', '')
+            parteer = float(parteer)
+            if parteer not in errs:
+                errs.append(parteer)
+    return errs
+
+
+def read_config(args):
+    with open(args.config, 'r') as stream:
+        config = yaml.safe_load(stream)
+        for key, value in config.items():
+            args[key] = value
+
+    return args
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ECAPA_trainer Multi corpus")
+    parser.add_argument('--config',
+                        type=str,
+                        default="config.yml",
+                        help='Configuration file')
+
+    args = parser.parse_args()
+    args = read_config(args)
+    args = init_args(args)
---
+++ train_ecapa_multi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=fbank_multi
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+# #SBATCH --constraint=GPURAM_Min_24GB
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=8GB
+# #SBATCH --cpus-per-task=8
+#SBATCH --output=%x_output.log
+#SBATCH --error=%x_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+python3 trainECAPAModelMulti.py
+
+conda deactivate
