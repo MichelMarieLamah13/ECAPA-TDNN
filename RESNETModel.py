7696b80254aeff27d87d41c68c1bd2271ee6cee1
miguel <lamahmichelmarie@gmail.com>
2024-02-27 17:57:05 +0100

    27 Fev 2024 17:57

--- RESNETModel.py
+++ RESNETModel.py
@@ -81,10 +81,9 @@ class RESNETModel(nn.Module):
             index += len(labels)
             top1 += prec
             loss += nloss.detach().cpu().numpy()
-            print(f"{time.strftime('%m-%d %H:%M:%S')} [{epoch:2d}] Lr: {lr:.5f}, "
-                  f"Training: {100 * (num / loader.__len__()):.2f}%, "
-                  f"Loss: {loss / num:.5f}, ACC: {top1 / index * len(labels):2.2f}%\r", flush=True)
-        sys.stdout.write("\n")
+            print(time.strftime("%m-%d %H:%M:%S") + \
+                  " [%2d] Lr: %5f, Training: %.2f%%, " % (epoch, lr, 100 * (num / loader.__len__())) + \
+                  " Loss: %.5f, ACC: %2.2f%%" % (loss / num, top1 / index * len(labels)), flush=True)
         return loss / num, lr, top1 / index * len(labels)
 
     def eval_network(self, eval_list, eval_path, n_cpu=5):
