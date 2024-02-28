400d3343408077cae22338bdc4b2b6f957e925c8
miguel <lamahmichelmarie@gmail.com>
2024-02-28 19:49:26 +0100

    28 Fev 2024 19:49

--- train_resnet_multi.sh
+++ train_resnet_multi.sh
@@ -4,6 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
+#SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --cpus-per-task=4
 #SBATCH --output=%x_output.log
 #SBATCH --error=%x_error.log
