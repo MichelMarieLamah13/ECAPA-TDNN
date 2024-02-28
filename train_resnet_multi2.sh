5a213243699ef254dd917577795a1d5584679e86
miguel <lamahmichelmarie@gmail.com>
2024-02-28 14:53:41 +0100

    28 Fev 2024 14:53

---
+++ train_resnet_multi2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=resnet_multi
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=8GB
+#SBATCH --cpus-per-task=4
+#SBATCH --output=%x_output.log
+#SBATCH --error=%x_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+python3 trainRESNETModelMulti2.py
+#python3 -m pdb trainRESNETModelMulti2.py
+
+conda deactivate
