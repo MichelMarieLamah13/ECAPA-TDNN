0e50d5efaf6e0f3c78aaae732379f6e7eeb198d1
miguel <lamahmichelmarie@gmail.com>
2024-03-22 21:23:41 +0100

    22 Mars 2024 21:23

--- train_resnet_multi_ncl_1.sh
+++ train_resnet_multi_ncl_1.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# resnet_multi_ncl (original), tmp_1(voxceleb),
-# tmp_2(voxceleb + cn_celeb), tmp_2_1 Moyenne(voxceleb + cn_celeb)
-# tmp_3(voxceleb + vietnam_celeb), tmp_3_1 Moyenne(voxceleb + vietnam_celeb)
-# tmp_4 Moyenne(voxceleb + vietnam_celeb + cn_celeb)
 #SBATCH --job-name=resnet_multi_ncl_1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
