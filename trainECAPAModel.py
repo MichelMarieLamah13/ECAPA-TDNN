a103279f4ed1056e4705a9b67588a59de25552ea
miguel <lamahmichelmarie@gmail.com>
2024-03-22 18:58:44 +0100

    22 Mars 2024 18:58

--- trainECAPAModel.py
+++ trainECAPAModel.py
@@ -5,6 +5,7 @@ This is the main code of the ECAPATDNN project, to define the parameters and bui
 import argparse, glob, os, torch, warnings, time
 import sys
 
+import yaml
 from torch.utils.data import DataLoader
 
 from tools import *
@@ -26,58 +27,30 @@ def init_eer(score_path):
     return errs
 
 
+def read_config(args):
+    with open(args.config, 'r') as stream:
+        config = yaml.safe_load(stream)
+        for key, item in config.items():
+            setattr(args, key, item['value'])
+
+    return args
+
+
 if __name__ == "__main__":
-    base_path = "./../kiwano-project/recipes/resnet"
     parser = argparse.ArgumentParser(description="ECAPA_trainer")
     # Training Settings
-    parser.add_argument('--num_frames', type=int, default=200,
-                        help='Duration of the input segments, eg: 200 for 2 second')
-    parser.add_argument('--max_epoch', type=int, default=80, help='Maximum number of epochs')
-    parser.add_argument('--batch_size', type=int, default=400, help='Batch size')
-    parser.add_argument('--n_cpu', type=int, default=20, help='Number of loader threads')
-    parser.add_argument('--test_step', type=int, default=1, help='Test and save every [test_step] epochs')
-    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
-    parser.add_argument("--lr_decay", type=float, default=0.97, help='Learning rate decay every [test_step] epochs')
-
-    # Training and evaluation path/lists, save path
-    parser.add_argument('--train_list', type=str, default=f"{base_path}/db/voxceleb2/train_list.txt",
-                        help='The path of the training list, '
-                             'https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/train_list.txt')
-    parser.add_argument('--train_path', type=str, default=f"{base_path}/db/voxceleb2/wav",
-                        help='The path of the training data, eg:"data/voxceleb2" in my case')
-    parser.add_argument('--eval_list', type=str, default=f"{base_path}/db/voxceleb1/veri_test2.txt",
-                        help='The path of the evaluation list: veri_test2.txt, list_test_all2.txt, list_test_hard2.txt'
-                             'veri_test2.txt comes from '
-                             'https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/veri_test2.txt')
-    parser.add_argument('--eval_path', type=str, default=f"{base_path}/db/voxceleb1/wav",
-                        help='The path of the evaluation data, eg:"data/voxceleb1/" in my case')
-    parser.add_argument('--musan_path', type=str, default=f"{base_path}/db/musan_split",
-                        help='The path to the MUSAN set, eg:"data/musan_split" in my case')
-    parser.add_argument('--rir_path', type=str, default=f"{base_path}/db/rirs_noises/RIRS_NOISES/simulated_rirs",
-                        help='The path to the RIR set, eg:"data/RIRS_NOISES/simulated_rirs" in my case')
-    parser.add_argument('--save_path', type=str, default="exps/exp1", help='Path to save the score.txt and models')
-    parser.add_argument('--initial_model', type=str, default="", help='Path of the initial_model')
-
-    # Model and Loss settings
-    parser.add_argument('--C', type=int, default=1024, help='Channel size for the speaker encoder')
-    parser.add_argument('--m', type=float, default=0.2, help='Loss margin in AAM softmax')
-    parser.add_argument('--s', type=float, default=30, help='Loss scale in AAM softmax')
-    parser.add_argument('--n_class', type=int, default=5994, help='Number of speakers')
-    parser.add_argument('--feat_type', type=str, default='fbank', help='Type of features: fbank, wav2vec2, wavlm')
-    parser.add_argument('--feat_dim', type=int, default=80, help='Dim of features: fbank(80), wav2vec2(768)')
-    parser.add_argument('--model_name', type=str, default='facebook/wav2vec2-base-960h',
-                        help='facebook/wav2vec2-base-960h, facebook/wav2vec2-large-960h'
-                             'facebook/wav2vec2-large-robust-ft-libri-960h, facebook/wav2vec2-large-960h-lv60-self')
-    parser.add_argument('--is_2d', dest='is_2d', action='store_true', help='2d learneable weight')
-
-    # Command
-    parser.add_argument('--eval', dest='eval', action='store_true', help='Only do evaluation')
+    parser.add_argument('--config',
+                        type=str,
+                        default="config_ecapa.yml",
+                        help='Configuration file')
 
     # Initialization
     warnings.simplefilter("ignore")
     # torch.multiprocessing.set_start_method('spawn', force=True)
     torch.multiprocessing.set_sharing_strategy('file_system')
+
     args = parser.parse_args()
+    args = read_config(args)
     args = init_args(args)
 
     # Define the data loader
