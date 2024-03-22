0aa213df899cf2c5bcef3dad24635450e77fa944
miguel <lamahmichelmarie@gmail.com>
2024-03-22 21:21:10 +0100

    22 Mars 2024 21:21

--- RESNETModel.py
+++ RESNETModel.py
@@ -218,12 +218,6 @@ class RESNETModelMulti(nn.Module):
                 speaker_embedding = self.speaker_encoder(data.to(self.device), aug=True)
                 nloss, prec = speaker_loss_(speaker_embedding, labels)
 
-                # to_consider = [2]
-                # if idx_loss in to_consider:
-                #     total_loss += nloss / len(to_consider)  # Accumulate loss
-                #     total_prec += prec  # Accumulate precision
-                #     total_index += len(labels)  # Accumulate index for averaging precision
-
                 total_loss += nloss / len(self.speaker_loss)  # Accumulate loss
                 total_prec += prec  # Accumulate precision
                 total_index += len(labels)  # Accumulate index for averaging precision
@@ -369,6 +363,149 @@ class RESNETModelMulti2(nn.Module):
                   " [%2d] Lr: %5f, Training: %.2f%%, " % (epoch, lr, 100 * (num / loader.__len__())) + \
                   " Loss: %.5f, ACC: %2.2f%%" % (loss / num, top1 / index * n_label), flush=True)
         return loss / num, lr, top1 / index * n_label
+
+    def eval_network(self, eval_list, eval_path, n_cpu=5):
+        self.eval()
+        files = []
+        embeddings = {}
+        lines = open(eval_list).read().splitlines()
+        print("BEGIN split", flush=True)
+        for line in tqdm.tqdm(lines):
+            _, part1, part2 = line.split()
+            files.append(part1)
+            files.append(part2)
+        setfiles = list(set(files))
+        setfiles.sort()
+        print("END split", flush=True)
+
+        print("BEGIN embeddings", flush=True)
+        emb_dataset = EmbeddingsDataset(setfiles, eval_path)
+        emb_loader = DataLoader(emb_dataset, batch_size=64, num_workers=n_cpu, collate_fn=collate_fn)
+        for idx, batch in tqdm.tqdm(enumerate(emb_loader, start=1), total=len(emb_loader)):
+            all_file, all_data_1, all_lengths_1, all_data_2 = batch
+            for i in range(len(all_file)):
+                file = all_file[i]
+                length_1 = all_lengths_1[i]
+                data_1 = all_data_1[i][:, :length_1]
+                data_1 = data_1.to(self.device)
+                data_2 = all_data_2[i].to(self.device)
+                with torch.no_grad():
+                    embedding_1 = self.speaker_encoder(data_1, aug=False)
+                    embedding_2 = self.speaker_encoder(data_2, aug=False)
+                    embedding_1 = F.normalize(embedding_1, p=2, dim=1)
+                    embedding_2 = F.normalize(embedding_2, p=2, dim=1)
+                embeddings[file] = [embedding_1, embedding_2]
+
+        scores, labels = [], []
+        print("END embeddings", flush=True)
+
+        print("BEGIN scores", flush=True)
+        for line in tqdm.tqdm(lines):
+            part0, part1, part2 = line.split()
+            embedding_11, embedding_12 = embeddings[part1]
+            embedding_21, embedding_22 = embeddings[part2]
+            # Compute the scores
+            score_1 = torch.mean(torch.matmul(embedding_11, embedding_21.T))  # higher is positive
+            score_2 = torch.mean(torch.matmul(embedding_12, embedding_22.T))
+            score = (score_1 + score_2) / 2
+            score = score.detach().cpu().numpy()
+            scores.append(score)
+            labels.append(int(part0))
+
+        print("END scores", flush=True)
+
+        print("BEGIN final score", flush=True)
+        # Coumpute EER and minDCF
+        EER, minDCF = 0, 0
+        if len(scores) > 0 and len(labels) > 0:
+            EER = tuneThresholdfromScore(scores, labels, [1, 0.1])[1]
+            fnrs, fprs, thresholds = ComputeErrorRates(scores, labels)
+            minDCF, _ = ComputeMinDcf(fnrs, fprs, thresholds, 0.05, 1, 1)
+        else:
+            print("Pas de ligne correcte", flush=True)
+
+        print("END final score", flush=True)
+
+        return EER, minDCF
+
+    def save_parameters(self, path, delete=False):
+        if delete:
+            folder = os.path.dirname(path)
+            old_files = glob.glob(f'{folder}/model_0*.model')
+            for file in old_files:
+                os.remove(file)
+        torch.save(self.state_dict(), path)
+
+    def load_parameters(self, path):
+        self_state = self.state_dict()
+        loaded_state = torch.load(path, map_location=self.device)
+        for name, param in loaded_state.items():
+            origname = name
+            if name not in self_state:
+                name = name.replace("module.", "")
+                if name not in self_state:
+                    print(f"{origname} is not in the model.", flush=True)
+                    continue
+            if self_state[name].size() != loaded_state[origname].size():
+                print(f"Wrong parameter length: {origname}, model: {self_state[name].size()}, loaded: "
+                      f"{loaded_state[origname].size()}", flush=True)
+                continue
+            self_state[name].copy_(param)
+
+
+class RESNETModelMulti3(nn.Module):
+    def __init__(self, lr, lr_decay, C, n_class, m, s, test_step, **kwargs):
+        super(RESNETModelMulti3, self).__init__()
+        # ResNet
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.speaker_encoder = ResNet(BasicBlock).to(self.device)
+        # Classifier
+        n_class = n_class.strip().split('\n')
+        n_class = [nc.strip() for nc in n_class if len(nc.strip()) > 0]
+
+        self.speaker_loss = {}
+        for i, n_class_ in enumerate(n_class):
+            n_class_ = int(n_class_.strip())
+            self.speaker_loss[str(i)] = AAMsoftmax(n_class=n_class_, m=m, s=s).to(self.device)
+
+        self.speaker_loss = nn.ModuleDict(self.speaker_loss)
+
+        self.optim = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=2e-5)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optim, step_size=test_step, gamma=lr_decay)
+        print(time.strftime("%m-%d %H:%M:%S") + " Model para number = %.2f" % (
+                sum(param.numel() for param in self.speaker_encoder.parameters()) / 1024 / 1024))
+
+    def train_network(self, epoch, loader):
+        self.train()
+        # Update the learning rate based on the current epoch
+        self.scheduler.step(epoch - 1)
+        index, top1, loss = 0, 0, 0
+        lr = self.optim.param_groups[0]['lr']
+        num = 1
+        for num, batch in tqdm.tqdm(enumerate(loader, start=1), total=len(loader)):
+            self.optim.zero_grad()
+
+            for idx_loss, speaker_loss_ in self.speaker_loss.items():
+                idx_loss = int(idx_loss)
+                data = batch[idx_loss * 2]  # Adjust indexing to access data and labels correctly
+                labels = torch.LongTensor(batch[idx_loss * 2 + 1]).to(self.device)
+
+                speaker_embedding = self.speaker_encoder(data.to(self.device), aug=True)
+                nloss, prec = speaker_loss_(speaker_embedding, labels)
+
+                nloss.backward()
+                self.optim.step()
+
+                n_label = len(labels)
+                index += n_label
+                top1 += prec
+                loss += nloss.detach().cpu().numpy() / len(self.speaker_loss)
+
+            print(time.strftime("%m-%d %H:%M:%S") + \
+                  " [%2d] Lr: %5f, Training: %.2f%%, " % (epoch, lr, 100 * (num / loader.__len__())) + \
+                  " Loss: %.5f, ACC: %2.2f%%" % (loss / num, top1 / index * len(labels)), flush=True)
+
+        return loss / num, lr, top1 / index * len(labels)
 
     def eval_network(self, eval_list, eval_path, n_cpu=5):
         self.eval()
---
+++ train_resnet_multi_ncl_1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# resnet_multi_ncl (original), tmp_1(voxceleb),
+# tmp_2(voxceleb + cn_celeb), tmp_2_1 Moyenne(voxceleb + cn_celeb)
+# tmp_3(voxceleb + vietnam_celeb), tmp_3_1 Moyenne(voxceleb + vietnam_celeb)
+# tmp_4 Moyenne(voxceleb + vietnam_celeb + cn_celeb)
+#SBATCH --job-name=resnet_multi_ncl_1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=32GB
+#SBATCH --constraint=GPURAM_Min_16GB
+#SBATCH --cpus-per-task=8
+#SBATCH --output=%x_output.log
+#SBATCH --error=%x_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+python3 trainRESNETModelMulti_ncl_1.py
+
+conda deactivate
