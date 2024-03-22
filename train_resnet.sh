8c51ae82e1ef1caf88e15286c7a722cbbe523f01
miguel <lamahmichelmarie@gmail.com>
2024-03-22 18:21:39 +0100

    22 Mars 2024 18:21

--- train_resnet.sh
+++ train_resnet.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=resnet_cc
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-# #SBATCH --constraint=GPURAM_16GB
+#SBATCH --constraint=GPURAM_Min_16GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=8
