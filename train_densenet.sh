d160fc3ac07a9b4faa6558355639f0ab2d299197
miguel <lamahmichelmarie@gmail.com>
2024-04-11 18:20:40 +0200

    11 Avril 2024 18:20

--- train_densenet.sh
+++ train_densenet.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=densenet_vc
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-# #SBATCH --constraint=GPURAM_16GB
+#SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=8
