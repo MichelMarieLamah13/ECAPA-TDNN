d63ece879a33319118060d28c8d31a76a3264428
miguel <lamahmichelmarie@gmail.com>
2024-04-15 09:12:39 +0200

    15 Avril 2024 09:12

--- train_densenet.sh
+++ train_densenet.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --time=7-00:00:00
-#SBATCH --exclude=eris,appolon
+#SBATCH --exclude=eris,apollon
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=8
 #SBATCH --output=%x_output.log
