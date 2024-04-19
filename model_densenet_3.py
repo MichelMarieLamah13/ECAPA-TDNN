d8a3d849e2208e2a77a87544ab3ea185b7bef589
miguel <lamahmichelmarie@gmail.com>
2024-04-19 15:03:06 +0200

    19 Avril 2024 15:03

--- model_densenet_3.py
+++ model_densenet_3.py
@@ -227,6 +227,7 @@ class DenseNet(nn.Module):
         self.features.add_module('norm5', nn.BatchNorm2d(num_features))
 
         # Linear layer
+        self.pooling_mode = pooling_mode
         self.fc = nn.Linear(num_features, emb_size)
         self.bn2 = nn.BatchNorm1d(emb_size)
         # Official init from torch repo.
