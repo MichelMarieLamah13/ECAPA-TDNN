113ae3ac9120a075d209a72b529bc2662f44409d
miguel <lamahmichelmarie@gmail.com>
2024-04-19 16:00:08 +0200

    19 Avril 2024 16:00

--- model_densenet_3.py
+++ model_densenet_3.py
@@ -228,7 +228,6 @@ class DenseNet(nn.Module):
 
         # Linear layer
         self.pooling_mode = pooling_mode
-        pooling_size = 2 if self.pooling_mode in ['statistical', 'std_skew', 'std_kurtosis'] else 1
         self.fc = nn.Linear(
             num_features * math.ceil(features_per_frame * (0.5 ** ((len(block_config) - 1) + 2))),
             emb_size)
