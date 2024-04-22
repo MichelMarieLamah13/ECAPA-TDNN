c97eb861d1388c9fa2e1dc54a79b27253d514fb5
mmlamah <lamahmichelmarie@gmail.com>
2024-04-22 12:34:18 +0200

    22 avril 2024 12:34

--- train_densenet_3.sh
+++ train_densenet_3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# JobNames v2: densenet_3(voxceleb), densenet_cc_3 (cn-celeb), densenet_vc_3 (vietnam-celeb)
-#SBATCH --job-name=densenet_vc_3
+# Using pooling not adaptative
+# JobNames: densenet_3_std(voxceleb), densenet_cc_3_std (cn-celeb), densenet_vc_3_std (vietnam-celeb)
+# JobNames: densenet_3_std_2(voxceleb), densenet_cc_3_std_2 (cn-celeb), densenet_vc_3_std_2 (vietnam-celeb)
+#SBATCH --job-name=densenet_vc_3_std_2
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=GPURAM_Min_12GB
@@ -15,9 +17,11 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 trainDENSENETModel_3.py --config config_densenet_vietnam_celeb_3.yml
-# python3 trainDENSENETModel_3.py --config config_densenet_cn_celeb_3.yml
-# python3 trainDENSENETModel_3.py --config config_densenet_3.yml
+# python3 trainDENSENETModel_3.py --config config_densenet_vietnam_celeb_3_std.yml
+# python3 trainDENSENETModel_3.py --config config_densenet_cn_celeb_3_std.yml
+# python3 trainDENSENETModel_3.py --config config_densenet_3_std.yml
+
+python3 trainDENSENETModel_3.py --config config_densenet_vietnam_celeb_3_std_2.yml
 
 
 conda deactivate
