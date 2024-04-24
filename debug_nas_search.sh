24fce76d42b0d11cc800aa591c353115153b751d
miguel <lamahmichelmarie@gmail.com>
2024-04-24 23:43:33 +0200

    24 Avril 2024 23:43

--- debug_nas_search.sh
+++ debug_nas_search.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 ##SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --time=7-00:00:00
-#SBATCH --mem=128GB
+#SBATCH --mem=256GB
 #SBATCH --cpus-per-task=8
 #SBATCH --output=%x_output.log
 #SBATCH --error=%x_error.log
