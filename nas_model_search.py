547928bb6321ee26df8f467fc75ec472a16a6892
miguel <lamahmichelmarie@gmail.com>
2024-04-25 01:46:15 +0200

    25 Avril 2024 01:46

--- nas_model_search.py
+++ nas_model_search.py
@@ -1,8 +1,62 @@
 import math
 import torch.nn.functional as F
+import torchaudio
+
 from nas_operations import *
 from nas_utils import Genotype
 from nas_utils import gumbel_softmax, drop_path
+
+
+class PreEmphasis(torch.nn.Module):
+
+    def __init__(self, coef: float = 0.97):
+        super().__init__()
+        self.coef = coef
+        self.register_buffer(
+            'flipped_filter', torch.FloatTensor([-self.coef, 1.]).unsqueeze(0).unsqueeze(0)
+        )
+
+    def forward(self, input: torch.tensor) -> torch.tensor:
+        input = input.unsqueeze(1)
+        input = F.pad(input, (1, 0), 'reflect')
+        return F.conv1d(input, self.flipped_filter).squeeze(1)
+
+
+class FbankAug(nn.Module):
+
+    def __init__(self, freq_mask_width=(0, 8), time_mask_width=(0, 10)):
+        self.time_mask_width = time_mask_width
+        self.freq_mask_width = freq_mask_width
+        super().__init__()
+
+    def mask_along_axis(self, x, dim):
+        original_size = x.shape
+        batch, fea, time = x.shape
+        if dim == 1:
+            D = fea
+            width_range = self.freq_mask_width
+        else:
+            D = time
+            width_range = self.time_mask_width
+
+        mask_len = torch.randint(width_range[0], width_range[1], (batch, 1), device=x.device).unsqueeze(2)
+        mask_pos = torch.randint(0, max(1, D - mask_len.max()), (batch, 1), device=x.device).unsqueeze(2)
+        arange = torch.arange(D, device=x.device).view(1, 1, -1)
+        mask = (mask_pos <= arange) * (arange < (mask_pos + mask_len))
+        mask = mask.any(dim=1)
+
+        if dim == 1:
+            mask = mask.unsqueeze(2)
+        else:
+            mask = mask.unsqueeze(1)
+
+        x = x.masked_fill_(mask, 0.0)
+        return x.view(*original_size)
+
+    def forward(self, x):
+        x = self.mask_along_axis(x, dim=2)
+        x = self.mask_along_axis(x, dim=1)
+        return x
 
 
 class MixedOp(nn.Module):
@@ -82,6 +136,14 @@ class NASSEARCH(nn.Module):
     def __init__(self, C, num_classes, layers, primitives,
                  steps=4, multiplier=4, stem_multiplier=3, drop_path_prob=0.0):
         super(NASSEARCH, self).__init__()
+
+        self.torchfbank = torch.nn.Sequential(
+            PreEmphasis(),
+            torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_fft=512, win_length=400, hop_length=160, \
+                                                 f_min=20, f_max=7600, window_fn=torch.hamming_window, n_mels=80),
+        )
+        self.specaug = FbankAug()
+
         self._C = C
         self._num_classes = num_classes
         self._layers = layers
@@ -119,7 +181,7 @@ class NASSEARCH(nn.Module):
 
     def new(self):
         model_new = NASSEARCH(self._C, self._embed_dim, self._layers,
-                            self.PRIMITIVES, drop_path_prob=self.drop_path_prob).cuda()
+                              self.PRIMITIVES, drop_path_prob=self.drop_path_prob).cuda()
         for x, y in zip(model_new.arch_parameters(), self.arch_parameters()):
             x.data.copy_(y.data)
         return model_new
