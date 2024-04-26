71efdb16e8ce655d7dd3fdd79fec20bce4137e16
miguel <lamahmichelmarie@gmail.com>
2024-04-26 19:28:33 +0200

    26 Avril 2024 19:28

--- nas_model_search.py
+++ nas_model_search.py
@@ -208,8 +208,9 @@ class NASSEARCH(nn.Module):
                 else:
                     weights = gumbel_softmax(F.log_softmax(self.alphas_normal, dim=-1))
             s0, s1 = s1, cell(s0, s1, weights, self.drop_path_prob)
-        v = self.global_pooling(s1)
-        v = v.view(v.size(0), -1)
+        v = F.relu(s1, inplace=True)
+        v = self.global_pooling(v)
+        v = torch.flatten(v, 1)
         # if not self.training:
         #     return v
 
