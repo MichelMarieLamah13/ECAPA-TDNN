7855ef6ace6ff188d0f69833265d6f44ed529bb7
miguel <lamahmichelmarie@gmail.com>
2024-04-26 10:49:04 +0200

    26 Avril 2024 10:49

--- debug_densenet.sh
+++ debug_densenet.sh
@@ -17,6 +17,9 @@ conda activate ecapa_tdnn
 # python3 -m pdb trainDENSENETModel.py --config config_densenet_vietnam_celeb.yml
 # python3 -m pdb trainDENSENETModel.py --config config_densenet_vietnam_celeb_2.yml
 
-python3 -m pdb trainDENSENETModel_3.py --config config_densenet_vietnam_celeb_3_std.yml
+# python3 -m pdb trainDENSENETModel_3.py --config config_densenet_vietnam_celeb_3_std.yml
+
+python3 -m pdb trainDENSENETModel.py --config config_densenet_vietnam_celeb_2_2.yml
+
 
 conda deactivate
