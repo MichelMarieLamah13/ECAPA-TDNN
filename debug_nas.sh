46aee44ea9c423541fc732bfae865a2d721ce6cd
miguel <lamahmichelmarie@gmail.com>
2024-04-30 12:05:23 +0200

    30 Avril 2024 12:05

---
+++ debug_nas.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=debug
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+##SBATCH --constraint=GPURAM_Min_12GB
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=32GB
+#SBATCH --cpus-per-task=8
+#SBATCH --output=%x_output.log
+#SBATCH --error=%x_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+python3 -m pdb trainNASModel.py --config config_nas_vietnam_celeb.yml
+
+conda deactivate
