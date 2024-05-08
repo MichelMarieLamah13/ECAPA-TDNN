cf85a708db545d0fc36a9b70caeabcbafb684e7f
miguel <lamahmichelmarie@gmail.com>
2024-05-08 07:10:01 +0200

    08 Mai 2024 07:09

--- train_nas_search_2.sh
+++ train_nas_search_2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Job names: nas_search(voxceleb) nas_search_vc(vietnam-celeb), nas_search_cc(cn-celeb)
-#SBATCH --job-name=nas_search_cc
+#SBATCH --job-name=nas_search_vc_2
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --time=7-00:00:00
-#SBATCH --exclude=eris,apollon,helios
+##SBATCH --exclude=eris,apollon,helios
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=8
 #SBATCH --output=%x_output.log
