1318b0a01919ac25bacd24a11e0c5cfbceffb3d8
miguel <lamahmichelmarie@gmail.com>
2024-05-13 11:21:27 +0200

    13 Mai 2024 11:21

--- train_resnet.sh
+++ train_resnet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Job names: resnet(voxceleb) resnet_vc(vietnam-celeb), resnet_cc(cn-celeb)
 # Job names: resnet_f_vc (finetuner vietnam-celeb), resnet_f_cc (finetuner cn-celeb)
-#SBATCH --job-name=eval
+#SBATCH --job-name=resnet_vox_ft_vt_vox
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 ##SBATCH --constraint=GPURAM_Min_12GB
