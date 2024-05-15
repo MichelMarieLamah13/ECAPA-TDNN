23a3aeb0e8d4df3c041219e0d4cf0c7b45b03b7f
miguel <lamahmichelmarie@gmail.com>
2024-05-15 05:00:11 +0200

    15 Mai 2024 05:00

--- train_resnet.sh
+++ train_resnet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Job names: resnet(voxceleb) resnet_vc(vietnam-celeb), resnet_cc(cn-celeb)
 # Job names: resnet_f_vc (finetuner vietnam-celeb), resnet_f_cc (finetuner cn-celeb)
-#SBATCH --job-name=resnet_1cl_ft_cn_vt
+#SBATCH --job-name=resnet_1cl_ft_cn
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 ##SBATCH --constraint=GPURAM_Min_12GB
@@ -19,8 +19,8 @@ conda activate ecapa_tdnn
 # python3 trainRESNETModel.py --config config_resnet.yml
 # python3 trainRESNETModel.py --config config_resnet_cn_celeb.yml
 # python3 trainRESNETModel.py --config config_resnet_vietnam_celeb.yml
-python3 trainRESNETModel.py --config config_resnet_finetuner_vietnam_celeb.yml
-# python3 trainRESNETModel.py --config config_resnet_finetuner_cn_celeb.yml
+# python3 trainRESNETModel.py --config config_resnet_finetuner_vietnam_celeb.yml
+python3 trainRESNETModel.py --config config_resnet_finetuner_cn_celeb.yml
 # python3 -m pdb trainRESNETModel.py
 
 conda deactivate
