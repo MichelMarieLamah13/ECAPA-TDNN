ae93c19e6e25116fb99dd7d346ab7a9a1ef93a8c
miguel <lamahmichelmarie@gmail.com>
2024-05-16 11:06:25 +0200

    16 Mai 2024 11:06

--- train_resnet.sh
+++ train_resnet.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --time=7-00:00:00
-##SBATCH --exclude=eris,apollon,helios
+#SBATCH --exclude=eris,apollon,helios
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=8
 #SBATCH --output=%x_output.log
