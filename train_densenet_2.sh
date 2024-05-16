37daf88655fb0902543479d58c79feeecb20bd2d
miguel <lamahmichelmarie@gmail.com>
2024-05-16 08:36:43 +0200

    16 Mai 2024 08:36

--- train_densenet_2.sh
+++ train_densenet_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # JobNames v2: densenet_2(voxceleb), densenet_cc_2 (cn-celeb), densenet_vc_2 (vietnam-celeb)
 # JobNames v2: densenet_2_std(voxceleb), densenet_cc_2_std (cn-celeb), densenet_vc_2_std (vietnam-celeb)
-#SBATCH --job-name=densenet_2
+#SBATCH --job-name=densenet_cc_2
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 ##SBATCH --constraint=GPURAM_Min_12GB
@@ -18,8 +18,8 @@ source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
 # python3 trainDENSENETModel.py --config config_densenet_vietnam_celeb_2.yml
-# python3 trainDENSENETModel.py --config config_densenet_cn_celeb_2.yml
-python3 trainDENSENETModel.py --config config_densenet_2.yml
+python3 trainDENSENETModel.py --config config_densenet_cn_celeb_2.yml
+# python3 trainDENSENETModel.py --config config_densenet_2.yml
 
 # python3 trainDENSENETModel.py --config config_densenet_vietnam_celeb_2_std.yml
 
