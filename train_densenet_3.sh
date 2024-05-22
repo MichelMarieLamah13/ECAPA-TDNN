608ea4a1555919c88890eed09cdaa18e9344fab8
miguel <lamahmichelmarie@gmail.com>
2024-05-22 11:37:38 +0200

    22 Mai 2024 11:37

--- train_densenet_3.sh
+++ train_densenet_3.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=densenet_3_std_cc
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --constraint=GPURAM_Min_12GB
+##SBATCH --constraint=GPURAM_Min_12GB
 #SBATCH --time=7-00:00:00
 #SBATCH --exclude=helios,apollon,eris
 #SBATCH --mem=32GB
