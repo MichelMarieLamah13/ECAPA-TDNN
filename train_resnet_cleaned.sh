8091f9bbcb8afb0a4fbeff21f96c0b4e7162d602
miguel <lamahmichelmarie@gmail.com>
2024-06-07 10:57:07 +0200

    07 Juin 2024 10:57

---
+++ train_resnet_cleaned.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Job names: resnet_cleaned_vox(voxceleb) resnet_cleaned_vc(vietnam-celeb), resnet_cleaned_cc(cn-celeb)
+#SBATCH --job-name=resnet_cleaned_vc
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+##SBATCH --constraint=GPURAM_Min_16GB
+#SBATCH --time=7-00:00:00
+##SBATCH --exclude=eris,apollon,helios
+#SBATCH --mem=32GB
+#SBATCH --cpus-per-task=8
+#SBATCH --output=%x_%j_output.log
+#SBATCH --error=%x_%j_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+# python3 trainRESNETModel.py --config config_resnet_cleaned.yml
+# python3 trainRESNETModel.py --config config_resnet_cn_celeb_cleaned.yml
+python3 trainRESNETModel.py --config config_resnet_vietnam_celeb_cleaned.yml
+
+
+conda deactivate
