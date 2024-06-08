22234d16ee9ba11bb1c21fe1f8707b434f12d6f3
miguel <lamahmichelmarie@gmail.com>
2024-06-08 19:08:55 +0200

    08 Juin 2024 19:08

--- train_densenet_3_cleaned.sh
+++ train_densenet_3_cleaned.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Using pooling not adaptative
 # JobNames: densenet_cleaned_vox(voxceleb), densenet_cleaned_cc (cn-celeb), densenet_cleaned_vc (vietnam-celeb)
-#SBATCH --job-name=densenet_cleaned_cc
+#SBATCH --job-name=densenet_cleaned_vox
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=GPURAM_Min_12GB
@@ -18,8 +18,8 @@ source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
 # python3 trainDENSENETModel_3.py --config config_densenet_vietnam_celeb_3_std_cleaned.yml
-python3 trainDENSENETModel_3.py --config config_densenet_cn_celeb_3_std_cleaned.yml
-# python3 trainDENSENETModel_3.py --config config_densenet_3_std_cleaned.yml
+# python3 trainDENSENETModel_3.py --config config_densenet_cn_celeb_3_std_cleaned.yml
+python3 trainDENSENETModel_3.py --config config_densenet_3_std_cleaned.yml
 
 
 conda deactivate
